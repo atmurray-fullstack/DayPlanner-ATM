e71696f07b2f398fa42ab23f533484ba992e45a4
Andrew Murray <atmurray@bellsouth.net>
2020-03-31 12:34:20 -0400

    created dayplanner page through DOM manipulation using jQuery

--- script.js
+++ script.js
@@ -1,11 +1,72 @@
 
+var currentTime = moment().format('LT');
+
+console.log(currentTime);
+var now = currentTime.toString();
+now = now.split("");
+now.splice(2, 4);
+hour = now[0] + [1];
+amPm = now[2] + now[3];
+console.log(hour);
+console.log(amPm);
+
+
 var h1El = $('<h1>');
-h1El.text('Hey There')
+h1El.text(moment().format('ll'))
 //fill in todayDate h1
 $('#todayDate').append(h1El)
+$('#todayDate').addClass('mb-3')
 
 
-var newRow=$('<div>');
-newRow.addClass('row');
 
-var timeBlock
+
+
+for (let i = 0; i < 9; i++) {
+    var newRow = $('<div>');
+    newRow.addClass('row text-center mx-auto mt-1');
+    newRow.attr('id', 'row' + i)
+    console.log(newRow.attr('id'))
+
+
+    var timeBlock = $('<div>');
+    timeBlock.addClass('col-2 timeBlock rowcont');
+    timeBlock.text('timeBlock')
+    timeBlock.attr('id', 'timeBlock' + i)
+    timeBlock.height('15vh');
+
+    var notesBlock = $('<div>');
+    notesBlock.addClass('col-8 notesBlock rowcont');
+    notesBlock.text('notesBlock');
+    notesBlock.attr('notesBlock', 'row' + i);
+    notesBlock.height('15vh');
+
+    var saveBlock = $('<div>');
+    saveBlock.addClass('col-1 saveBlock rowcont')
+    saveBlock.text('saveBlock');
+    saveBlock.attr('id', 'saveBlock' + i)
+    saveBlock.height('15vh');
+
+    if (i < 3) {
+        count = i + 9
+        count = count + ' AM'
+        timeBlock.text(count);
+    } else if (i == 3) {
+        count = i + 9
+        count = count + ' PM'
+        timeBlock.text(count);
+    } else if (i > 3 && i < 9) {
+        count = i - 3
+        count = count + ' PM'
+        timeBlock.text(count);
+    }
+   
+
+
+    $('#main').append(newRow);
+    newRow.append(timeBlock);
+    newRow.append(notesBlock);
+    newRow.append(saveBlock);
+
+}
+
+
